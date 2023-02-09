456ae0c4a73d639ffb2c4165fa92813bfdade23b
mohamed28875701 <mohamedbenhadjlakhal@gmail.com>
2023-02-09 20:13:43 +0100

    can bundle css and pictures

--- webpack.config.js
+++ webpack.config.js
@@ -4,14 +4,21 @@ module.exports = {
   mode:"development",
   entry: './src/index.js',
   devtool:"inline-source-map",
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Development',
-    }),
-  ],
+  
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-    clean:true,
   },
+  module:{
+    rules:[
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ]
+  }
 };
