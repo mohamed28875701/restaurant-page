f6c6fcf65c3e2a1bb2d053447435f521a0b07366
mohamed28875701 <mohamedbenhadjlakhal@gmail.com>
2023-02-12 17:15:18 +0100

    added the home div

---
+++ src/home.js
@@ -0,0 +1,19 @@
+export default setupHome;
+function setupHome(){
+    const home=document.createElement("div");
+    home.setAttribute("id","homediv");
+    const welcome=document.createElement("h2");
+    welcome.setAttribute("id","headline");
+    welcome.textContent="Welcome To Breadington";
+    const imgPlaceholder=document.createElement("div");
+    imgPlaceholder.setAttribute("id","img");
+    imgPlaceholder.classList.add("img");
+    const desc=document.createElement("p");
+    desc.textContent="This is breadington,the best sandwich restaurant in town where we will serve you the most outstanding and mouth watering sandwiches";
+    desc.setAttribute("id","desc");
+    home.appendChild(welcome);
+    home.appendChild(imgPlaceholder);
+    home.appendChild(desc);
+    home.classList.add("main");
+    return home;
+}
