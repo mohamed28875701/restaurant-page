56dd79363ed121d39a42cc02eb80e726aadb156c
mohamed28875701 <mohamedbenhadjlakhal@gmail.com>
2023-02-12 18:43:19 +0100

    added functionality to the buttons

--- src/setupSite.js
+++ src/setupSite.js
@@ -1,5 +1,8 @@
 export default setup;
-import "./styles/site.css"
+import "./styles/site.css";
+import setupHome from "./home";
+import setupMenu from "./menu";
+import setupContact from "./contact";
 function Navbtns(){
     let wrapper=document.createElement("div");
     let home=document.createElement("button");
@@ -8,6 +11,69 @@ function Navbtns(){
     home.setAttribute("id","homebtn");
     menu.setAttribute("id","menubtn");
     contact.setAttribute("id","contactbtn");
+    home.textContent="home";
+    menu.textContent="menu";
+    contact.textContent="contacts";
+    menu.addEventListener("click",(e)=>{
+        if(e.target.getAttribute("data-status")==="active")
+        return ;
+        const current=document.querySelector('[data-status="active"]');
+        current.setAttribute("data-status","inactive");
+        current.remove();
+        let now;
+        if(e.target.textContent==="home"){
+            now=setupHome();
+        }
+        else if(e.target.textContent==="menu"){
+            now=setupMenu();
+        }
+        else{
+            now =setupContact();
+        }
+        now.setAttribute("data-status","active");
+        const content=document.getElementById("content");
+        content.appendChild(now);
+    })
+    home.addEventListener("click",(e)=>{
+        if(e.target.getAttribute("data-status")==="active")
+        return ;
+        const current=document.querySelector('[data-status="active"]');
+        current.setAttribute("data-status","inactive");
+        current.remove();
+        let now;
+        if(e.target.textContent==="home"){
+            now=setupHome();
+        }
+        else if(e.target.textContent==="menu"){
+            now=setupMenu();
+        }
+        else{
+            now =setupContact();
+        }
+        now.setAttribute("data-status","active");
+        const content=document.getElementById("content");
+        content.appendChild(now);
+    })
+    contact.addEventListener("click",(e)=>{
+        if(e.target.getAttribute("data-status")==="active")
+        return ;
+        const current=document.querySelector('[data-status="active"]');
+        current.setAttribute("data-status","inactive");
+        current.remove();
+        let now;
+        if(e.target.textContent==="home"){
+            now=setupHome();
+        }
+        else if(e.target.textContent==="menu"){
+            now=setupMenu();
+        }
+        else{
+            now =setupContact();
+        }
+        now.setAttribute("data-status","active");
+        const content=document.getElementById("content");
+        content.appendChild(now);
+    })
     home.classList.add("btns");
     menu.classList.add("btns");
     contact.classList.add("btns");
@@ -15,11 +81,10 @@ function Navbtns(){
     wrapper.appendChild(home);
     wrapper.appendChild(menu);
     wrapper.appendChild(contact);
-    home.textContent="home";
-    menu.textContent="menu";
-    contact.textContent="contacts";
+    
     return wrapper;
 }
+
 function setupHeader(){
     let header=document.createElement("div");
     let title=document.createElement("h2");
@@ -40,14 +105,12 @@ function setupFooter(){
 function setup(){
     const content=document.getElementById("content");
     const body=document.getElementById("body");
-    
-    
     body.classList.add("body");
     content.classList.add("content");
-    
     let header=setupHeader();
     let footer=setupFooter();
     content.appendChild(header);
+    content.appendChild(setupHome());
     content.appendChild(footer);
     
 }
